d25b16d3998359a6ce22a97484808627f1eec7ed
Sanjal KC <sanjalkcs2030@gmail.com>
2026-04-09 14:46:50 +1000

    Show_expense func is fixed

--- expensetracker.py
+++ expensetracker.py
@@ -27,24 +27,32 @@ class ExpenseTracker:
         if list is None:
             self.list = []
 
+    def show_expense(self):
+        for x in self.list:
+            print(f"{x.tracker}.) {x.name}\t{x.amount}")
+
     def add_expense(self):
         exp_input = input("Please enter your expenses: (coffee 50): ").lower().split()
         self.list.append((Expenses(*exp_input)))
-
-        # self.list.append({'Expense': })
 
     def edit_expense(self):
         pass
 
     def remove_expense(self):
+        d1.show_expense()
         inp = int(input("Select the expense number for deleting: "))
-        for i, x in enumerate(self.arr):
-            if inp == x["id"]:
-                self.arr.pop(i)
+        for x in self.list:
+            if inp == x.id:
+                del x
 
     def menu(self):
         while True:
-            menu_item = ["Abdd Expense", "Edit Expense", "Remove Expense"]
+            menu_item = [
+                "Add Expense",
+                "Edit Expense",
+                "Remove Expense",
+                "Show Expense",
+            ]
             for i, x in enumerate(menu_item):
                 print(f"{i+1} {x}")
             choice = input("Enter your choice: ").strip().lower().split()[0]
@@ -54,15 +62,14 @@ class ExpenseTracker:
             elif choice in ["2", "edit"]:
                 self.edit_expense()
             elif choice in ["3", "remove"]:
-                self.show_expense()
                 self.remove_expense()
+
+            elif choice in ["4", "show"]:
+                self.show_expense()
+
             else:
                 print("Quitting: ")
                 break
-
-    def show_expense(self):
-        for x in self.list:
-            print(f"{x.tracker}) {x.name}---{x.amount}")
 
 
 d1 = ExpenseTracker()
